3da196f0e0c21e740ac7652c564419ff7b124004
arisia <starlights2283@gmail.com>
2023-11-30 11:54:59 +0900

    add first files

---
+++ src/app.java
@@ -0,0 +1,15 @@
+interface Account {
+    public void deposit(int amount);
+    public void withdraw(int amount);
+    public int getBalance();
+}
+
+interface Stock {
+    public void buy(int amount);
+    public void sell(int amount);
+    public int getQuantity();
+}
+
+public class app {
+
+}
