fa7bc0fb309ae177631454c5b51d81221be7a440
arisia <starlights2283@gmail.com>
2023-11-30 12:27:59 +0900

    add textgenerator

--- src/app.java
+++ src/app.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 interface Account {
     int getBalance();
@@ -8,9 +11,68 @@ interface Account {
     void withdraw(int amount);
 }
 
+interface Card {
+    String getCardType();
+    String getCardPassword();
+}
+
+class CreditCard implements Card {
+    private String cardPassword;
+    private int loanAmount;
+    private String repaymentDate;
+
+    CreditCard(String cardPassword) {
+        this.cardPassword = cardPassword;
+        this.loanAmount = 0;
+        this.repaymentDate = "";
+    }
+
+    @Override
+    public String getCardType() {
+        return "Credit Card";
+    }
+
+    @Override
+    public String getCardPassword() {
+        return this.cardPassword;
+    }
+
+    public int getLoanAmount() {
+        return this.loanAmount;
+    }
+
+    public String getRepaymentDate() {
+        return this.repaymentDate;
+    }
+
+    public void loan(int amount, String date) {
+        this.loanAmount += amount;
+        this.repaymentDate = date;
+    }
+}
+
+class CheckCard implements Card {
+    private String cardPassword;
+
+    CheckCard(String cardPassword) {
+        this.cardPassword = cardPassword;
+    }
+
+    @Override
+    public String getCardType() {
+        return "Check Card";
+    }
+
+    @Override
+    public String getCardPassword() {
+        return this.cardPassword;
+    }
+}
+
 class BankAccount implements Account {
     private int balance;
     private String password;
+    private Card card;
 
     BankAccount(String password) {
         this.balance = 0;
@@ -39,6 +101,20 @@ class BankAccount implements Account {
     public boolean checkPassword(String password) {
         return this.password.equals(password);
     }
+
+    public void issueCard(String cardType, String cardPassword) {
+        if (cardType.equals("Credit Card")) {
+            this.card = new CreditCard(cardPassword);
+        } else if (cardType.equals("Check Card")) {
+            this.card = new CheckCard(cardPassword);
+        } else {
+            System.out.println("Invalid card type");
+        }
+    }
+
+    public Card getCard() {
+        return this.card;
+    }
 }
 
 class Stock {
@@ -66,6 +142,7 @@ class Stock {
 
     static class Manager {
         private static HashMap<String, BankAccount> accounts = new HashMap<>();
+        private static HashMap<String, String> users = new HashMap<>();
 
         static Stock createStock(String name, int code, int price) {
             Stock stock = new Stock(name, code, price);
@@ -89,6 +166,47 @@ class Stock {
 
         static BankAccount getAccount(String id) {
             return accounts.get(id);
+        }
+
+        static boolean register(String id, String password, String address, String cardType) {
+            if (users.containsKey(id)) {
+                System.out.println("User already exists");
+                return false;
+            }
+
+            users.put(id, password);
+            BankAccount account = createAccount(id, password);
+            account.issueCard(cardType, password);
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("user.txt", true))) {
+                writer.write(id + " " + password + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("account.txt", true))) {
+                writer.write(id + " " + "000-0000-0000" + " " + password + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }
+
+        static boolean unregister(String id, String password) {
+            if (!users.containsKey(id)) {
+                System.out.println("User does not exist");
+                return false;
+            }
+
+            if (!users.get(id).equals(password)) {
+                System.out.println("Invalid password");
+                return false;
+            }
+
+            users.remove(id);
+            accounts.remove(id);
+            return true;
         }
     }
 
@@ -121,11 +239,20 @@ class Stock {
                 System.out.println("Not logged in");
             }
         }
-    }
-}
 
-public class app {
-    public static void main(String[] args){
-
+        static void printCardInfo(String id) {
+            BankAccount account = loggedInAccounts.get(id);
+            if (account != null) {
+                Card card = account.getCard();
+                System.out.println("Card Type: " + card.getCardType());
+                if (card instanceof CreditCard) {
+                    CreditCard creditCard = (CreditCard) card;
+                    System.out.println("Loan Amount: " + creditCard.getLoanAmount());
+                    System.out.println("Repayment Date: " + creditCard.getRepaymentDate());
+                }
+            } else {
+                System.out.println("Not logged in");
+            }
+        }
     }
 }
